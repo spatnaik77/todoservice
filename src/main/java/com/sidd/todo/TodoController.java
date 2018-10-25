461bbfe401787805c60d3358e25eb1ace0e09908
Siddharth Patnaik <siddharth.patnaik@walmartlabs.com>
2018-10-25 23:44:35 +0530

    Initial commit

---
+++ src/main/java/com/sidd/todo/TodoController.java
@@ -0,0 +1,19 @@
+package com.sidd.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    public TodoController()
+    {
+
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello from todo service \n";
+    }
+
+}

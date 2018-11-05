da119e891faddcdfc9c3bb98534eb03d74c17d26
Siddharth Patnaik <spatnaik77@gmail.com>
2018-11-05 15:55:42 +0530

    testing

---
+++ src/test/java/com/sidd/todoservice/TodoserviceApplicationTest.java
@@ -0,0 +1,24 @@
+/*package com.sidd.todoservice;
+
+import com.sidd.todo.DemoApplication;
+import com.sidd.todo.TodoController;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class TodoserviceApplicationTest {
+
+    @Autowired
+    TodoController todoController;
+
+	@Test
+	public void contextLoads()
+	{
+        Assert.assertNotNull("todoController is null", todoController);
+	}
+}*/

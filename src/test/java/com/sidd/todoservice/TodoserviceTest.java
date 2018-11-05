2caf4fdf3b810e64af3e5f221551db0a9c7bc0fd
Siddharth Patnaik <spatnaik77@gmail.com>
2018-11-05 15:24:15 +0530

    added few tests and commented

---
+++ src/test/java/com/sidd/todoservice/TodoserviceTest.java
@@ -0,0 +1,19 @@
+/*package com.sidd.todoservice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TodoserviceTest {
+
+    @Test
+    public void init()
+    {
+        Assert.assertEquals(1, 1);
+    }
+    @Test
+    public void init1()
+    {
+        Assert.assertEquals(1, 1);
+    }
+}
+*/

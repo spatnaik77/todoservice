abde8030dcfa92ee24c852a8a996ab85965c93ad
Siddharth Patnaik <spatnaik77@gmail.com>
2018-11-07 14:40:36 +0530

    trying

--- src/it/java/com/sidd/todoservice/IntegrationTest.java
+++ src/it/java/com/sidd/todoservice/IntegrationTest.java
@@ -11,9 +11,6 @@ public class IntegrationTest
 
         String url = "https://siddharthtodoservice-it.azurewebsites.net/";
         RestAssured.given().when().get(url).then().statusCode(200);
-
-
-        
     }
 
 

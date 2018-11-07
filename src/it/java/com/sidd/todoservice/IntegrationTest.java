0f50f34f7e6cab968e93f19eda2a8d7ab8a0d7f1
Siddharth Patnaik <spatnaik77@gmail.com>
2018-11-07 13:41:00 +0530

    Integration tests added

---
+++ src/it/java/com/sidd/todoservice/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.sidd.todoservice;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Test;
+
+public class IntegrationTest
+{
+
+    @Test
+    public void basicPingTest() {
+
+        String url = "https://siddharthtodoservice-it.azurewebsites.net/";
+        RestAssured.given().when().get(url).then().statusCode(200);
+    }
+
+
+}

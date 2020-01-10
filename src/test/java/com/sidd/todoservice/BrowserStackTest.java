692346cd5f4d9168a3d4553f00a52279a4869c8f
Siddharth Patnaik <spatnaik77@gmail.com>
2020-01-10 16:35:43 +0530

    Added a simple test

--- src/test/java/com/sidd/todoservice/BrowserStackTest.java
+++ src/test/java/com/sidd/todoservice/BrowserStackTest.java
@@ -3,7 +3,9 @@ package com.sidd.todoservice;
 import com.sidd.todo.dao.TodoItemInMemoryRepository;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -14,8 +16,6 @@ public class BrowserStackTest {
     public static final String USERNAME = "siddharthpatnaik1";
     public static final String AUTOMATE_KEY = "gPSwmUrHKZof8bxiSuDr";
     public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-
-    WebDriver driver = null;
 
     @BeforeClass
     public static void init()
@@ -33,5 +33,13 @@ public class BrowserStackTest {
         caps.setCapability("name", "Bstack-[Java] Sample Test");
 
         WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("http://www.google.com/ncr");
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("BrowserStack");
+        element.submit();
+
+        System.out.println(driver.getTitle() + " Done...");
+        driver.quit();
     }
 }
